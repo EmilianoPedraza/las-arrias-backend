9dc8ff8af7a17dddd1b7213638b940dec3e84d21
Emiliano <j.p.e.23.11.98@gmail.com>
2025-01-30 15:03:39 -0300

    modificacion de subesquema horariosAtencion-defaoult:'cerrado'

--- app/models/schemas/subSchemas/horariosAtencion.ts
+++ app/models/schemas/subSchemas/horariosAtencion.ts
@@ -1,6 +1,6 @@
-import { Schema } from "mongoose";
+import { Schema } from 'mongoose';
 
-//* Tipo de hora: formato "HH:MM-HH:MM" o 'cerrado'
+//* Tipo de hora: formato 'HH:MM-HH:MM' o 'cerrado'
 type Hora = `${string}:${string}-${string}:${string}` | 'cerrado'
 
 //* Función de validación para asegurarse que el formato del horario sea válido
@@ -11,8 +11,8 @@ const validHs = (hs: Hora): boolean => {
 
 //* Esquema para horarios de atención, que incluye mañana, tarde y noche
 export const horariosAtencionSchema = new Schema({
-    mañana: { type: String, validate: { validator: validHs, message: "Horario de mañana mal ingresado" } },
-    tarde: { type: String, validate: { validator: validHs, message: "Horario de tarde mal ingresado" } },
-    noche: { type: String, required: false, validate: { validator: validHs, message: "Horario de noche mal ingresado" } }
+    mañana: { type: String, default:'cerrado', validate: { validator: validHs, message: 'Horario de mañana mal ingresado' } },
+    tarde: { type: String, default:'cerrado', validate: { validator: validHs, message: 'Horario de tarde mal ingresado' } },
+    noche: { type: String, default:'cerrado', required: false, validate: { validator: validHs, message: 'Horario de noche mal ingresado' } }
 });
 
