f15c293fbc7007f83749afd5aaaa5adfae57d18b
Emiliano <j.p.e.23.11.98@gmail.com>
2025-05-06 05:43:23 -0300

    se retoma etapa de desarrollo, commit para primer commit guardado en repositorio github

--- app/vars.ts
+++ app/vars.ts
@@ -1,8 +1,10 @@
 //solo para entendimiento de como funciona la url
-const password = 'ññ232311'
-const usuarioCluser = 'admin'
+// const password = 'ññ232311'
+// const usuarioCluser = 'admin'
 const db = 'Las-Arrias-Web-Server'
-const rutaMongoUsers:string = `mongodb+srv://${usuarioCluser}:${password}@las-arrias-web-server.onif1.mongodb.net/${db}?retryWrites=true&w=majority&appName=Las-Arrias-Web-Server`
+//? const rutaMongoUsers:string = `mongodb+srv://${usuarioCluser}:${password}@las-arrias-web-server.onif1.mongodb.net/${db}?retryWrites=true&w=majority&appName=Las-Arrias-Web-Server`
 
+
+const rutaMongoUsers: string = `mongodb://localhost:27017/${db}`
 
 export default rutaMongoUsers
