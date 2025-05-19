2e10549e5c7ca018f9512043db15b57ea5f8e53d
Emiliano <j.p.e.23.11.98@gmail.com>
2025-05-19 19:42:07 -0300

    no se caambio nada como tal, solaente se dejo lineas en blanco en los script de package json para identificar los comandos, ahora en modo desarrollo solo se usara mongoCompas ya que asi se configuraron las variables 2

--- app/config/connectDb.ts
+++ app/config/connectDb.ts
@@ -10,7 +10,7 @@ const MONGO_COMPAS_DB_LA = "mongodb://localhost:27017/Las-Arrias-Web-Server"
 
 const connectDb = async () => {
     try {
-        const rut = !isDev ? MONGO_COMPAS_DB_LA : MONGO_CLOUSER_LA
+        const rut = isDev ? MONGO_COMPAS_DB_LA : MONGO_CLOUSER_LA
 
         const conn = await mongoose.connect(rut as string)
         console.log(`connectDb-MongoDb-cluster-conectado: ${conn.connection.host}`);
